38f00ebe3011845483e27b73ce07d8195d7733e5
Theodore Bjernhed <uik1475@edu.kunskapsskolan.se>
2026-04-25 17:56:48 +0200

    feat: allow custom binds

--- README.md
+++ README.md
@@ -2,6 +2,3 @@
 
 A small script to bind/unbind a keybind based on the currently active window in Hyprland.
 
-Currently only works with kitty and my personal keybinding.
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
-use hyprland::{
-    bind,
-    dispatch::DispatchType,
-    event_listener::EventListener,
-    keyword::Keyword,
-};
+use hyprland::{event_listener::EventListener, keyword::Keyword};
 
+// kitty, SUPER, q, exec, uwsm app -- kitty
 fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let rest: &'static str = Box::leak(args.join(" ").into_boxed_str());
+    let (class, rest): (&'static str, &'static str) = rest.split_once(", ").unwrap();
+    let (modifiers, rest): (&'static str, &'static str) = rest.split_once(", ").unwrap();
+    let (key, action): (&'static str, &'static str) = rest.split_once(", ").unwrap();
+
+    dbg!(class, modifiers, key, action);
+
     let instance = hyprland::default_instance_panic();
     let mut listener = EventListener::new();
-    listener.add_active_window_changed_handler(|wevent| {
+    listener.add_active_window_changed_handler(move |wevent| {
         if let Some(wevent) = wevent {
-            if wevent.class == "kitty" {
-                Keyword::instance_set(instance, "unbind", "SUPER,q").unwrap();
-                println!("keyword unbind SUPER, q");
+            if wevent.class == class {
+                Keyword::instance_set(instance, "unbind", format!("{modifiers},{key}")).unwrap();
+                println!("keyword unbind {modifiers}, {key}");
             } else {
-                bind!(instance, SUPER, Key, "q" => Exec, "uwsm app -- kitty").unwrap();
-                println!("keyword bind SUPER, q, exec, uwsm app -- kitty");
+                Keyword::instance_set(instance, "bind", format!("{modifiers},{key},{action}"))
+                    .unwrap();
+                println!("keyword bind {modifiers}, {key}, {action}");
             }
         }
     });
